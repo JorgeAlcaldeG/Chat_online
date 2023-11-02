aeba376810d32ad6885cb152c7ca173afcfda623
Wilson-Alvarracin <100007104.joan23@fje.edu>
2023-11-02 22:27:36 +0100

    02/11/23 V0.1
    Comprobación de contraseña en un usuario de la base de datos

--- proc/proc_login.php
+++ proc/proc_login.php
@@ -2,23 +2,29 @@
     require 'conexion.php';
     $user = mysqli_real_escape_string($conn,$_POST['user']);
     $pwd = mysqli_real_escape_string($conn,$_POST['pwd']);
-
     //Consulta
-    mysqli_autocommit($conn, false);
-    
+
     $sql_login = "SELECT user_username, user_pwd from usuarios where user_username = ?";
     $stm_consulta = mysqli_stmt_init($conn);
     mysqli_stmt_prepare($stm_consulta, $sql_login);
     mysqli_stmt_bind_param($stm_consulta, "s", $user);
 
-    var_dump(mysqli_stmt_execute($stm_consulta));
+    mysqli_stmt_execute($stm_consulta);
+    $verif = mysqli_stmt_get_result($stm_consulta);
+    $res = mysqli_fetch_assoc($verif);
 
-    if (password_verify('qweQWE123', $pwd)) {
+
+    if (password_verify($pwd, $res['user_pwd'])) {
         echo 'Password is valid!';
         echo "<br>";
         echo "acceso al chat";
         session_start();
 
+
     } else {
+        echo $pwd;
+        echo "<br>";
+        echo $userpwd;
+        echo "<br>";
         echo 'Invalid password.';
     }
